df4dd28a4a1417e276736f5c25bef86c26aba9de
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-11-11 01:39:29 +0000

    Sample

--- src/mainProcess.ts
+++ src/mainProcess.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from "node:events";
-// import child_process from "node:child_process";
+import express from "express";
+import child_process from "node:child_process";
 
 export declare interface mainProcess {
   // Error emit
@@ -10,11 +11,35 @@ export declare interface mainProcess {
   emit(act: "error", data: any): boolean;
 };
 
+export type initjs_process = ({
+  status: "running"|"restarting",
+  childProcess: child_process.ChildProcess,
+}|{
+  status: "stoped"
+})&{
+  name: string,
+  childrens?: string[]
+};
+
 /**
  * Create process listen socket and http(s) server to maneger remotely process maneger
 */
 export class mainProcess extends EventEmitter {
+  processList: {[id: string]: initjs_process} = {};
   constructor() {
     super();
+    const app = express();
+    app.get("/", ({res}) => res.json(Object.keys(this.processList).reduce((mount, id) => {
+      const data = this.processList[id];
+      if (data.status === "stoped") mount[id] = {status: "stoped"};
+      else {
+        mount[id] = {
+          status: data.status,
+          pid: data.childProcess.pid,
+          childrensIDs: data.childrens
+        };
+      }
+      return mount;
+    }, {})));
   }
 }
